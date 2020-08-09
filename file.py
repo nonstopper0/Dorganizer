365ab407595a0dba1a1559ce745a02242c8124d2
Nathaniel Redmon <nonstopper0@gmail.com>
2020-08-09 14:08:55 -0600

    sorting code completed

--- file.py
+++ file.py
@@ -10,6 +10,10 @@ os.chdir(os.path.dirname(os.getcwd()))
 videos = ['.MP4', '.MOV', '.WMV', '.FLV', '.AVCHD']
 images = ['.JPEG', '.JPG', '.PNG', '.TIFF', '.BMP', '.RAW', '.ARW', '.NEF', '.WMF', '.TGA', '.JFIF', '.PSD', '.TIF', '.EPS']
 compressed = ['.7Z', '.ARJ', '.DEB', '.PKG', '.TAR.GZ', '.Z', '.RAR', '.RPM']
+os.mkdir(os.path.join(os.getcwd(), 'videos'))
+os.mkdir(os.path.join(os.getcwd(), 'images'))
+os.mkdir(os.path.join(os.getcwd(), 'compressed'))
+
 
 # a list of all the extensions in the folder, keeps track of what directories have been made
 extensions = []
@@ -19,25 +23,26 @@ i = 0
 for file in os.listdir(os.getcwd()):
     i += 1
     new_file = os.path.splitext(file)
-    extension_path = new_file[1].upper()
+    extension = new_file[1].upper()
+    file_path = os.path.join(os.getcwd(), file)
 
     # if the file isnt a folder
-    if (extension_path != ''):  
-        directory = None 
-        if extension_path in videos:
+    if (extension != ''):  
+        directory = False 
+        if extension in videos:
             directory = 'videos'
-        elif extension_path in images:
+        elif extension in images:
             directory = 'images' 
-        elif extension_path in compressed:
+        elif extension in compressed:
             directory = 'compressed'
-        
-        if directory: 
-            os.mkdir(os.path.join(os.getcwd(), directory))
+
+        if directory:
+            shutil.move(file_path, os.path.join(os.getcwd(), directory))
             
         # if extension_path not in extensions:
         #     extensions.append(extension_path)
         #     os.mkdir(os.path.join(os.getcwd(), directory))
 
-    print(f"{extension_path}\n{new_file}\n{i}\n\n")
+    print(f"{extension}\n{new_file}\n{i}\n\n")
 
 print(f"files found: {i}\nextensions found: {extensions}")
