8a46c12867029572e62c7bacb5a21952c8c6ff59
Nathaniel Redmon <nonstopper0@gmail.com>
2020-08-09 14:24:04 -0600

    fixed minor directory bug

--- file.py
+++ file.py
@@ -9,10 +9,15 @@ os.chdir(os.path.dirname(os.getcwd()))
 # list of extentions for sorting
 videos = ['.MP4', '.MOV', '.WMV', '.FLV', '.AVCHD']
 images = ['.JPEG', '.JPG', '.PNG', '.TIFF', '.BMP', '.RAW', '.ARW', '.NEF', '.WMF', '.TGA', '.JFIF', '.PSD', '.TIF', '.EPS']
-compressed = ['.7Z', '.ARJ', '.DEB', '.PKG', '.TAR.GZ', '.Z', '.RAR', '.RPM']
-os.mkdir(os.path.join(os.getcwd(), 'videos'))
-os.mkdir(os.path.join(os.getcwd(), 'images'))
-os.mkdir(os.path.join(os.getcwd(), 'compressed'))
+compressed = ['.7Z', '.ZIP', '.ARJ', '.DEB', '.PKG', '.TAR.GZ', '.Z', '.RAR', '.RPM']
+applications = ['.APP', '.EXE', '.MSI']
+music = ['.MP3', '.WAV', '.WMA', '.M4A', '.AAC']
+os.mkdir(os.path.join(os.getcwd(), 'Videos'))
+os.mkdir(os.path.join(os.getcwd(), 'Images'))
+os.mkdir(os.path.join(os.getcwd(), 'Music'))
+os.mkdir(os.path.join(os.getcwd(), 'Applications'))
+os.mkdir(os.path.join(os.getcwd(), 'Compressed'))
+os.mkdir(os.path.join(os.getcwd(), 'Files'))
 
 
 # a list of all the extensions in the folder, keeps track of what directories have been made
@@ -30,19 +35,29 @@ for file in os.listdir(os.getcwd()):
     if (extension != ''):  
         directory = False 
         if extension in videos:
-            directory = 'videos'
+            directory = 'Videos'
         elif extension in images:
-            directory = 'images' 
+            directory = 'Images' 
         elif extension in compressed:
-            directory = 'compressed'
+            directory = 'Compressed'
+        elif extension in music:
+            directory = 'Music'
+        elif extension in applications:
+            directory = 'Applications'
 
+        # file extension matched one of the specified formats and is being categorized
         if directory:
             shutil.move(file_path, os.path.join(os.getcwd(), directory))
+
+        # if file does not match with any categories listed
+        if directory == False: 
+            shutil.move(file_path, os.path.join(os.getcwd(), 'Files'))
             
         # if extension_path not in extensions:
         #     extensions.append(extension_path)
         #     os.mkdir(os.path.join(os.getcwd(), directory))
-
-    print(f"{extension}\n{new_file}\n{i}\n\n")
+    
+    elif (extension == ''):
+        print('folder found', file)
 
 print(f"files found: {i}\nextensions found: {extensions}")
