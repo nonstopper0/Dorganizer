2802c1bd340d152b574703a4fa324f0320f437ff
Nathaniel Redmon <nonstopper0@gmail.com>
2020-08-09 13:13:44 -0600

    file now scims through download folder and reads all extensions

--- file.py
+++ file.py
@@ -1,12 +1,27 @@
+import os
+import shutil
+from imported import *
+
 global file
 
-try:
-    file = open('Data.txt', 'r+')
+print('for the easiest method, please place the entire application folder within your downloads folder')
 
-except: 
-    file = open('Data.txt', 'w')
-    file.write(data)
+# move up the directory tree by one folder
+os.chdir(os.path.dirname(os.getcwd()))
 
-finally: 
-    print('done')
-    file.close()
+# a list of all the extensions in the folder
+extensions = ['.JPG']
+
+# read every file in the directory
+i = 0
+for file in os.listdir(os.getcwd()):
+    i += 1
+    new_file = os.path.splitext(file)
+    extension_path = new_file[1].upper()
+    # if the file isnt a folder
+    if (extension_path != ''):
+        if extension_path not in extensions:
+            extensions.append(extension_path)
+    print(f"{extension_path}\n{new_file}\n{i}\n\n")
+
+print(f"extensions found: {extensions)")
