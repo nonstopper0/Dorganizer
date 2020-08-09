e7e2d4c08d08044bf4a1f7b73f843833ccbd7a7d
Nathaniel Redmon <nonstopper0@gmail.com>
2020-08-09 13:44:09 -0600

    adding paramers for different file types

--- file.py
+++ file.py
@@ -1,16 +1,17 @@
 import os
 import shutil
-from imported import *
-
-global file
 
 print('for the easiest method, please place the entire application folder within your downloads folder')
 
 # move up the directory tree by one folder
 os.chdir(os.path.dirname(os.getcwd()))
 
-# a list of all the extensions in the folder
-extensions = ['.JPG']
+videos = ['.MP4', '.MOV', '.WMV', '.FLV', '.AVCHD']
+images = ['.JPEG', '.JPG', '.PNG', '.TIFF', '.BMP', '.RAW', '.ARW', '.NEF', '.WMF', '.TGA', '.JFIF', '.PSD', '.TIF', '.EPS']
+compressed = ['.7Z', '.ARJ', '.DEB', '.PKG', '.TAR.GZ', '.Z', '.RAR', '.RPM']
+
+# a list of all the extensions in the folder, keeps track of what directories have been made
+extensions = []
 
 # read every file in the directory
 i = 0
@@ -18,10 +19,21 @@ for file in os.listdir(os.getcwd()):
     i += 1
     new_file = os.path.splitext(file)
     extension_path = new_file[1].upper()
+
     # if the file isnt a folder
     if (extension_path != ''):
+        directory = False
+        if extension_path in videos:
+            directory = 'videos'
+        elif extension_path in images:
+            directory = 'images' 
+        elif extension_path in compressed:
+            directory = 'compressed'
+        elif extension_path in 
         if extension_path not in extensions:
             extensions.append(extension_path)
+            os.mkdir(os.path.join(os.getcwd(), extension_path[1:]))
+
     print(f"{extension_path}\n{new_file}\n{i}\n\n")
 
-print(f"extensions found: {extensions)")
+print(f"files found: {i}\nextensions found: {extensions}")
